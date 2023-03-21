d98b320e4d15ddba9435747d41d3bf73439d72c1
itxbasit <109472859+itxbasit@users.noreply.github.com>
2023-03-21 22:26:31 +0500

    Add files via upload

---
+++ Assignment No 7/app.js
@@ -0,0 +1,58 @@
+//      Chapter No  26-30
+
+//Question No 1
+// var num = +prompt("Enter a positive integer");
+// console.log("Number: " + num);
+// console.log("round off value: " + Math.round(num));
+// console.log("floor value: " + Math.floor(num));
+// console.log("ceil value: " + Math.ceil(num));
+
+// Question No 2
+// var num = +prompt("Enter a negative integer");
+// console.log("Number: " + num);
+// console.log("round off value: " + Math.round(num));
+// console.log("floor value: " + Math.floor(num));
+// console.log("ceil value: " + Math.ceil(num));
+
+// Question No 3
+// var num = +prompt("Enter a negative integer");
+// console.log(Math.abs(num));
+
+// Question No 4
+// var dice = Math.ceil(Math.random() * 6);
+// console.log("random dice value: " + dice);
+
+// Question No 5
+// var coin = Math.ceil(Math.random() * 2);
+// if(coin == 2){
+//     console.log(coin);
+//     console.log("random coin value: Heads");
+// }
+// else{
+//     console.log(coin);
+//     console.log("random coin value: Tails");
+// }
+
+// Question No 6
+// var randomNum = Math.ceil(Math.random() * 100);
+// console.log("random number between 1 and 100: " + randomNum);
+
+// Question No 7
+// var c = "";
+// var weight = prompt("Enter your weight in kilograms");
+// for(var i=0; i<weight.length; i++){
+//     if(weight[i] == "k"){
+//         c = i;
+//         break;
+//     }
+// }
+// console.log("The weight of user is " + weight.slice(0, c) + " kilograms.");
+
+// Question No 8
+// var secNum = 5;
+// var user = +prompt("Enter a number between 1 and 10");
+// if (secNum === user) {
+//   console.log("Your answer is correct");
+// } else {
+//   console.log("Try again");
+// }
